914f740bf3ced55c144313d307ab0dcb8442bb10
Gabriel Aguiar <72907265+Raiugami@users.noreply.github.com>
2021-08-17 21:26:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 import pandas as pd
+contatos_df = pd.read_excel("listateste.xlsx")
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import time
 import urllib
-
-from selenium.webdriver.common.keys import keys
-
-contatos_df = pd.read_excel ("lista1.xlsx")
-display(contatos_df)
 
 navegador = webdriver.Chrome()
 navegador.get("https://web.whatsapp.com/")
 
-while len(navegador.find_elements_by_id ("side")) < 1:
+while len(navegador.find_elements_by_id("side")) < 1:
     time.sleep(1)
 # Já estamos com o login feito no whatsapp web
 
 for i, mensagem in enumerate(contatos_df['Mensagem']):
-    pessoa = contatos_df.loc[i,"NOMES"]
-    numero = contatos_df.loc[i,"TELEFONES"]
+    pessoa = contatos_df.loc[i, "NOME"]
+    numero = contatos_df.loc[i, "TELEFONES"]
+    texto = urllib.parse.quote(mensagem)
     link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
+    navegador.get(link)
+    while len(navegador.find_elements_by_id("side")) < 1:
+        time.sleep(1)
+    navegador.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(Keys.ENTER)
+    
+    time.sleep(10)
+
+
