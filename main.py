cf20b81da56d0782910a5ff21806c421135ebcb3
Gabriel Aguiar <72907265+Raiugami@users.noreply.github.com>
2021-08-23 11:16:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 import pandas as pd
-contatos_df = pd.read_excel("listateste.xlsx")
-
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 import urllib
 
-navegador = webdriver.Chrome()
-navegador.get("https://web.whatsapp.com/")
+continuar = "S"
 
-while len(navegador.find_elements_by_id("side")) < 1:
-    time.sleep(1)
-# Já estamos com o login feito no whatsapp web
+while continuar != "N":
+    lista = str(input("Informe o numero da lista: "))
 
-for i, mensagem in enumerate(contatos_df['Mensagem']):
-    pessoa = contatos_df.loc[i, "NOME"]
-    numero = contatos_df.loc[i, "TELEFONES"]
-    texto = urllib.parse.quote(mensagem)
-    link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
-    navegador.get(link)
+    contatos_df = pd.read_excel(f"lista{lista}.xlsx")
+
+    navegador = webdriver.Chrome()
+    navegador.get("https://web.whatsapp.com/")
+
     while len(navegador.find_elements_by_id("side")) < 1:
         time.sleep(1)
-    navegador.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(Keys.ENTER)
-    
-    time.sleep(10)
+
+    # Já estamos com o login feito no whatsapp web
+
+    def dados():
+        numero = contatos_df.loc[i, "TELEFONES"]
+        texto = urllib.parse.quote(mensagem)
+        link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
+        navegador.get(link)
+        time.sleep(10)
+
+    def esperar():
+        while len(navegador.find_elements_by_id("side")) < 1:
+            time.sleep(1)
+        time.sleep(5)
+
+    def condicao():
+        if len(navegador.find_elements_by_xpath(
+                '//*[@id="app"]/div[1]/span[2]/div[1]/span/div[1]/div/div/div/div/div[2]/div/div/div')) > 0:
+            botao = navegador.find_element_by_xpath(
+                '//*[@id="app"]/div[1]/span[2]/div[1]/span/div[1]/div/div/div/div/div[2]/div/div/div')
+            botao.click()
+        elif len(navegador.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]/button')) > 0:
+            botzinho = navegador.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]/button')
+            botzinho.click()
 
 
+    for i, mensagem in enumerate(contatos_df['Mensagem']):
+
+        dados()
+
+        condicao()
+
+        esperar()
+
+        time.sleep(10)
+
+    continuar = str(input("Deseja continuar? [S/N]")).upper()
+
+    if continuar == "N":
+        break
